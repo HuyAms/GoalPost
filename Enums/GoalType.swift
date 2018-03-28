a3e1b9d85380d886054a31bedcc2baf63f9c1924
Daniel <dinhhuyams@gmail.com>
2018-03-28 10:58:38 +0300

    add goalType enum

---
+++ Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by iosdev on 28.3.2018.
+//  Copyright © 2018 Group-6. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType:String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
