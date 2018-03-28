808e5c2d602781ba64486a4387b2dc4fddc6ec6a
Daniel <dinhhuyams@gmail.com>
2018-03-28 10:38:47 +0300

    add coredata

--- Controllers/GoalsVC.swift
+++ Controllers/GoalsVC.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
  
